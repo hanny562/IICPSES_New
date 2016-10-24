addfd345d404f1ed88c44c05ca7dfcdafc73a029
hanny562 <hanny562@hotmail.com>
2016-10-24 09:52:22 +0800

    Add project files.

---
+++ IICPSES_New.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace IICPSES_New.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
